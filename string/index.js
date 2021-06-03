74be2a613ba1c039ed8c12306e51ff0b6efc7037
code Anushri-Deshmukh <anunick.deshmukh@gmail.com>
2021-06-03 15:10:22 +0530

    first commit

---
+++ string/index.js
@@ -0,0 +1,12 @@
+var str = new String("Typescript");
+console.log(str.charAt(1));
+console.log(str.split(""));
+console.log(str.indexOf("s"));
+console.log(str.replace('Type', 'Java'));
+console.log(str.trim());
+console.log(str.substr(4, str.length));
+console.log(str.substring(4, 10));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(1, 4));
+console.log(str.lastIndexOf('p'));
---
+++ string/index.ts
@@ -0,0 +1,13 @@
+let str= new String ("Typescript");
+
+console.log(str.charAt(1));
+console.log(str.split(""));
+console.log(str.indexOf("s"));
+console.log(str.replace('Type','Java'));
+console.log(str.trim());
+console.log(str.substr(str.length));
+console.log(str.substring(4,10));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(1,4))
+console.log(str.lastIndexOf('p'));
